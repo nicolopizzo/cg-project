1ca2979b28a1a8ab78f826142a42ad5d6ef857bb
Nicolò Pizzo <nicolopizzo@outlook.com>
2022-12-12 16:56:58 +0100

    - completed mouse interaction - bus animation - added objects

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 const models = [
     'palazzo',
     'strada',
+    'strada_casa',
+    'marciapiedi',
+    'prato',
     'bus'
 ]
 
@@ -31,60 +34,61 @@ async function main() {
 
         Materials = {...Materials, ...materials}
         Parts = {...Parts, ...parts}
-        // Parts.push(parts)
-        // Parts = Parts.flat()
     }
 
-    // Parts = Parts.flatMap(x => x)
-    // console.log(Parts.flatMap(x => x))
-    console.log(Parts)
 
     const cameraTarget = [0, 0, 0];
     // figure out how far away to move the camera so we can likely
     // see the object.
-    // const radius = m4.length(range) * 3;
-    const radius = 16;
-    let cameraPosition = m4.addVectors(cameraTarget, [0, 0, radius]);
+    let radius = 15;
 
     // Set zNear and dof to something hopefully appropriate
     // for the size of this object.
-    const zNear = radius / 1000;
+    const zNear = radius * 0.001;
     const dof = radius * 3;
 
     function degToRad(deg) {
         return deg * Math.PI / 180;
     }
 
-    console.log(Materials)
-    console.log(Parts)
-
     let u_world = m4.yRotation(Math.PI)
-    u_world = m4.xRotate(u_world, -0.2)
-    u_world = m4.yRotate(u_world, 0.2)
+    u_world = m4.xRotate(u_world, - Math.PI / 8)
+    // u_world = m4.yRotate(u_world, 0.2)
 
-    document.addEventListener("mousedown", (event) => {
+    console.log(u_world)
+
+    document.querySelector("#canvas").addEventListener("mousedown", (event) => {
         canMove = true;
         x = event.x;
     })
-    document.addEventListener("mouseup", () => {
+    document.querySelector("#canvas").addEventListener("mouseup", () => {
         canMove = false
     })
-    document.addEventListener("mousemove", (event) => {
+    document.querySelector("#canvas").addEventListener("mousemove", (event) => {
         if (!canMove) {
             return
         }
 
         const yMove = event.x - x
 
-        u_world = m4.yRotate(u_world, yMove * 0.003)
+        u_world = m4.yRotate(u_world, yMove * 0.005)
+        busTransformation = m4.yRotate(busTransformation, yMove * 0.005)
         x = event.x
-        y = event.y
-
     })
 
-    function render(time) {
-        time *= 0.001;  // convert to seconds
+    document.querySelector("#canvas").addEventListener("wheel", (event) => {
+        const dy = event.deltaY
+        if (dy < 0) {
+            radius = Math.max(radius-1, 11)
+        } else {
+            radius = Math.min(radius+1, 19)
+        }
+        console.log(radius)
+    })
 
+    let busTransformation = u_world
+
+    function render() {
         // console.log(u_world)
         webglUtils.resizeCanvasToDisplaySize(gl.canvas);
         gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
@@ -96,6 +100,7 @@ async function main() {
 
         const up = [0, 1, 0];
         // Compute the camera's matrix using look at.
+        const cameraPosition = m4.addVectors(cameraTarget, [0, 0, radius]);
         const camera = m4.lookAt(cameraPosition, cameraTarget, up);
 
         // cameraPosition = m4.dot(cameraPosition, [0.2, 0.2, 0.2])
@@ -115,16 +120,27 @@ async function main() {
 
         // calls gl.uniform
         webglUtils.setUniforms(meshProgramInfo, sharedUniforms);
-
+        // console.log(time)
         for (let model of models) {
             for (const {bufferInfo, material} of Parts[model]) {
                 // calls gl.bindBuffer, gl.enableVertexAttribArray, gl.vertexAttribPointer
-                // console.log(bufferInfo)
                 webglUtils.setBuffersAndAttributes(gl, meshProgramInfo, bufferInfo);
-                // calls gl.uniform
-                webglUtils.setUniforms(meshProgramInfo, {
-                    u_world,
-                }, material);
+
+                switch (model) {
+                    case 'bus':
+                        // const move = (Math.random() * 0.002 - 0.005)
+                        const move = -0.004
+                        busTransformation = m4.yRotate(busTransformation, move)
+
+                        webglUtils.setUniforms(meshProgramInfo, {
+                            u_world: busTransformation,
+                        }, material);
+                        break
+                    default:
+                        webglUtils.setUniforms(meshProgramInfo, {
+                            u_world,
+                        }, material);
+                }
                 // calls gl.drawArrays or gl.drawElements
                 webglUtils.drawBufferInfo(gl, bufferInfo);
             }
